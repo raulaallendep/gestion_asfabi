f500419e622d2cada6c1006e4c4d85cec58813e7
Raul Allende <raul.aallendep@gmail.com>
2018-06-29 18:39:07 -0400

    Creando el modelo solicitud-cliente en el servidor

---
+++ server/models/sc.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose'
+
+const { ObjectId } = Schema.Types
+
+const SCSchema = Schema({
+  code: { type: String, required: true },
+  no: { type: String },
+  state: { type: String, required: true },
+  client: { type: ObjectId, ref: 'CL', required: true },
+  contact: { type: ObjectId, ref: 'CCL', required: true },
+  created: { type: Date, required: true, default: new Date() },
+  expired: { type: Date },
+  description: { type: String, required: true },
+  items: [{ type: ObjectId, ref: 'ISC', required: true, default: [] }],
+  sps: [{ type: ObjectId, ref: 'SP' }],
+  user: { type: ObjectId, ref: 'User', required: true }
+})
+
+export default mongoose.model('SC', SCSchema)
